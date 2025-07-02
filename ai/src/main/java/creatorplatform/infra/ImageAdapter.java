d3b9169dd7fb9a2bb695aa6bc07f328886cbe00b
minhyuklee <leeminhyuk12@naver.com>
2025-07-03 02:03:17 +0900

    feat(AI) - fix 어노테이션 누락

--- ai/src/main/java/creatorplatform/infra/ImageAdapter.java
+++ ai/src/main/java/creatorplatform/infra/ImageAdapter.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+@Component
 public class ImageAdapter extends AbstractOpenAiAdapter{
 
     private final RestTemplate restTemplate = new RestTemplate();
