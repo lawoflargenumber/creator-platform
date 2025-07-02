37d60b7b73334d1b4b4bf0fa3b7b9d19a49253e5
jingu.jeon <wlsrn158@gmail.com>
2025-07-02 15:20:51 +0900

    Update JwtAuthenticationFilter.java

--- gateway/src/main/java/creatorplatform/security/JwtAuthenticationFilter.java
+++ gateway/src/main/java/creatorplatform/security/JwtAuthenticationFilter.java
@@ -24,7 +24,8 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
         "/auth/register",
         "/auth/login",
         "/auth/refresh",
-        "/actuator"
+        "/actuator",
+        "/" 
     );
     
     @Override
